9be4420697db4a1ede7fce3d8e4cf4cf686732df
Melina Liz <u202216058@upc.edu.pe>
2025-12-02 22:32:21 -0500

    feat: update backend

--- FoodStock/obj/Debug/net9.0/FoodStock.AssemblyInfo.cs
+++ FoodStock/obj/Debug/net9.0/FoodStock.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodStock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ab15996dbfb96324e76eb607dfa18dbd732d4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e7e5be6b7a2264c10e561cae9c8289e2f9fb29")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodStock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodStock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
